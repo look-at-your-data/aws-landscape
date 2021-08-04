e3774f73264154843a4f824469537dcd90392f4c
shantanu.das <thoughtworks_shantanudas@mercedesme.onmicrosoft.com>
2021-08-04 13:24:04 +0530

    #7 SD: Initial commit for cloudwatch

---
+++ modules/cloudwatch/main.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_metric_alarm" "Low_Disk_Space_kafkaInstance" {
+  alarm_name                = "Low_Disk_Space_kafkaInstance"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "disk_used_percent"
+  namespace                 = "CWAgent"
+
+  dimensions {
+      InstanceId    = "i-xxxxxxxxxxxxxxxxx"
+      ImageId       = "your-image-id"
+      InstanceType  = "your-instance-type"
+      path          = "/"
+      device        = "your-device"
+      fstype        = "xfs"
+
+  }
+
+  period                    = "60"
+  statistics                = "Maximum"
+  threshold                 = "90" 
+  alarm_description         = "Disk usage for / is high"
+  insufficient_data_actions = []
+  actions_enabled           = true
+  alarm_actions             = ["arn:aws:sns:xxxxxx"]
+  ok_actions                = ["arn:aws:sns:xxxxxx"]
+}
