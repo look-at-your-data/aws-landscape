c4409f0c836bfa4d80c5fcaafc64d14ae7a89989
shantanu.das <thoughtworks_shantanudas@mercedesme.onmicrosoft.com>
2021-08-05 11:06:11 +0530

    KA,SD #7: added only master branch on ci crcle, add monitoring components

--- components/kafka_monitoring/main.tf
+++ components/kafka_monitoring/main.tf
@@ -2,38 +2,18 @@ terraform {
   backend "s3" {}
 }
 
-provider "aws" {
-  region  = "${var.aws_region}"
-  version = "~> 2.0"
-}
 
 
-data "terraform_remote_state" "base_networking" {
+data "terraform_remote_state" "monitoring" {
   backend = "s3"
   config {
-    key    = "base_networking.tfstate"
+    key    = "monitoring.tfstate"
     bucket = "tw-dataeng-${var.cohort}-tfstate"
     region = "${var.aws_region}"
   }
 }
 
-data "terraform_remote_state" "bastion" {
-  backend = "s3"
-  config {
-    key    = "bastion.tfstate"
-    bucket = "tw-dataeng-${var.cohort}-tfstate"
-    region = "${var.aws_region}"
-  }
-}
 
-data "terraform_remote_state" "training_emr_cluster" {
-  backend = "s3"
-  config {
-    key    = "training_emr_cluster.tfstate"
-    bucket = "tw-dataeng-${var.cohort}-tfstate"
-    region = "${var.aws_region}"
-  }
-}
 
 module "cloudwatch" {
   source = "../../modules/cloudwatch"
--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,30 +1,28 @@
+
 resource "aws_cloudwatch_metric_alarm" "Disk_Space_kafkaInstance" {
   alarm_name                = "Disk_Space_kafkaInstance"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
-  metric_name               = "disk_used_percent"
+  metric_name               = "DiskSpaceUtilization"
   namespace                 = "CWAgent"
 
-#   alarm_name                = "Disk_Space_"-${var.alertName}""
-#   comparison_operator       = var.operator
-#   evaluation_periods        = var.period
-#   metric_name               = "disk_used_percent"
-#   namespace                 = "CWAgent"
 
-  dimensions {
-      InstanceId    = "i-01fdc617e6e5016e1"
+
+  dimensions = {
+      InstanceId    = "i-01fdc617e6e5016e1" #Please do a data source and remove the hardcode
+      # InstanceId  = data.aws_instance.kafka_instance.
       path          = "/"
       device        = "your-device"
       fstype        = "xfs"
 
   }
 
-  period                    = "5"
-  statistics                = "Average"
+  period                    = "10"
+  statistic                = "Average"
   threshold                 = "50" 
   alarm_description         = "Disk usage for / is high"
   insufficient_data_actions = []
   actions_enabled           = true
-  alarm_actions             = ["arn:aws:cloudwatch:ap-southeast-1:483506802077:alarm:Kafka Disk Space Metrics"]
+  alarm_actions             = ["arn:aws:cloudwatch:ap-southeast-1:483506802077:alarm:Kafka Disk Space Metrics"] #variablize it 
   ok_actions                = ["arn:aws:cloudwatch:ap-southeast-1:483506802077:alarm:Kafka Disk Space Metrics"]
 }
